b53fca457863aef1725ce279435932ea3148e925
stuntmanmike87 <mich.perigaud@gmail.com>
2024-04-30 11:27:59 +0200

    Update deps (incl. Sf7.07) & recipes

--- config/bundles.php
+++ config/bundles.php
@@ -13,7 +13,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
-    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
