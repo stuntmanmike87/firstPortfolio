573bf67ad5a468f02dc1e20e44bcfe00225956a9
stuntmanmike87 <mich.perigaud@gmail.com>
2024-05-03 10:27:12 +0200

    Update sf/security-bdle recipe

---
+++ src/Entity/User.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/** @final */
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    final public const ROLE_USER = 'ROLE_USER';
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER)]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::STRING, unique: true)]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 50)]
+    private ?string $username = null; // private string $username;
+
+    #[ORM\Column(type: Types::STRING, unique: true)]
+    #[Assert\Email]
+    private ?string $email = null; // private string $email;
+
+    #[ORM\Column(type: Types::STRING)]
+    private ?string $password = null; // private string $password;
+
+    /** @var string[] */
+    #[ORM\Column(type: Types::JSON)]
+    private array $roles = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    #[\Override]
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->getUserIdentifier();
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    #[\Override]
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Returns the roles or permissions granted to the user for security.
+     */
+    #[\Override]
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+
+        // guarantees that a user always has at least one role for security
+        if ([] === $roles) {// if ($roles === ['']//if ($roles == null) {
+            $roles[] = self::ROLE_USER;
+        }
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param string[] $roles
+     */
+    public function setRoles(array $roles): void
+    {
+        $this->roles = $roles;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * {@inheritdoc}
+     */
+    #[\Override]
+    public function eraseCredentials(): void
+    {
+        // if you had a plainPassword property, you'd nullify it here
+        // $this->plainPassword = null;
+    }
+
+    /**
+     * @return array{int|null, string|null, string|null}
+     */
+    public function __serialize(): array
+    {
+        return [$this->id, $this->username, $this->password];
+    }
+
+    /**
+     * @param array{int|null, string, string} $data
+     */
+    public function __unserialize(array $data): void
+    {
+        [$this->id, $this->username, $this->password] = $data;
+    }
+}
