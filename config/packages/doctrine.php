32b3444c2e567130e6b3b23a5de6baae44358a79
stuntmanmike87 <mich.perigaud@gmail.com>
2024-05-03 11:31:57 +0200

    Update doctrine/doctrine-bdle recipe

--- config/packages/doctrine.php
+++ config/packages/doctrine.php
@@ -6,23 +6,31 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->extension('doctrine', [
+        'server_version' => '16',
+        'profiling_collect_backtrace' => '%kernel.debug%',
+        'use_savepoints' => true,
         'dbal' => [
             'url' => '%env(resolve:DATABASE_URL)%',
         ],
         'orm' => [
             'auto_generate_proxy_classes' => true,
             'enable_lazy_ghost_objects' => true,
+            'report_fields_where_declared' => true,
+            'validate_xml_mapping' => true,
             'naming_strategy' => 'doctrine.orm.naming_strategy.underscore_number_aware',
             'auto_mapping' => true,
             'mappings' => [
                 'App' => [
-                    'is_bundle' => false,
                     'type' => 'attribute',
+                    'is_bundle' => false,
                     'dir' => '%kernel.project_dir%/src/Entity',
                     'prefix' => 'App\Entity',
                     'alias' => 'App',
                 ],
             ],
+            'controller_resolver' => [
+                'auto_mapping' => true,
+            ]
         ],
     ]);
     if ($containerConfigurator->env() === 'test') {
